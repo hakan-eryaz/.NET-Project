956f328b85838d5ebfe0fec323f9d065a1104014
hakan-eryaz <hknerya@gmail.com>
2022-07-12 21:32:01 +0300

    repositoryler oluşturuldu.

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        Context c = new Context();
+        public void AddBlog(Blog blog)
+        {
+            c.Add(blog);
+        }
+
+        public Blog GetBlog(int id)
+        {
+           return c.Blogs.Find(id);
+        }
+
+        public List<Blog> GetBlogs()
+        {
+            return c.Blogs.ToList();
+        }
+
+        public void RemoveBlog(Blog blog)
+        {
+            c.Remove(blog);
+            c.SaveChanges();
+
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
