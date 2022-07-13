5f7a61677906d289d37c265c60f1d3c6b28a42ec
hakan-eryaz <hknerya@gmail.com>
2022-07-13 10:42:01 +0300

    tema üzerinde veriler listelendi.

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void AddBlog(Blog blog);
+        void RemoveBlog(Blog blog);
+        void UpdateCategory(Blog blog);
+        List<Blog> GetAllBlogs();
+        Blog GetBlog(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void AddBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetAllBlogs()
+        {
+            return _blogDal.GetAll();
+        }
+
+        public Blog GetBlog(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
