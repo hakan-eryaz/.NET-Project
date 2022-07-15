cc0708bcb9fa20788202f3f76694ec9e2437fe7d
hakan-eryaz <hknerya@gmail.com>
2022-07-15 19:38:04 +0300

    static view component kullanıldı

---
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICommentService
+    {
+        void AddComment(Comment comment);
+       // void RemoveBlog(Blog blog);
+        //void UpdateCategory(Blog blog);
+        List<Comment> GetAllComments(int id);
+        //Blog GetBlog(int id);
+        //List<Blog> GetBlogListWithCategory();
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke() {
+            var comment = new List<UserComment> {
+            new UserComment{
+            ID = 1,
+            UserName="Hakan"
+            },
+            new UserComment{
+            ID = 1,
+            UserName="Hakan"
+            },
+            new UserComment{
+            ID = 1,
+            UserName="Hakan"
+            },
+            new UserComment{
+            ID = 1,
+            UserName="Hakan"
+            },
+            new UserComment{
+            ID = 1,
+            UserName="Hakan"
+            }
+            };
+            return View(comment);
+        }
+
+    }
+}
