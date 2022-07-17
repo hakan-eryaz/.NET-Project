bb5187d3e7056b2f6f9213a4989b493d9010c1b6
hakan-eryaz <hknerya@gmail.com>
2022-07-17 18:52:07 +0300

    view component ile parametre kullanımı

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -15,6 +15,7 @@ namespace CoreDemo.Controllers
 
         public IActionResult BlogDetails(int id)
         {
+            ViewBag.Id = id;
             var values=bm.GetBlogByID(id);
             return View(values);
         }
